86de78cd9280d29cbc3a853a3e72d81dbf89cb50
arkegs <silvestre1602@gmail.com>
2025-03-13 01:05:44 -0300

    add wallpaper

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -26,7 +26,7 @@ export const GlobalStyle = createGlobalStyle`
     }
 
     body{
-        background: linear-gradient( rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.8)), url('https://cutewallpaper.org/21/football-stadium-background-hd/American-Football-Stadium-Nightfootball-Stadium-D-Hd-.jpg') no-repeat fixed center;
+        background: linear-gradient( rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.8)), url('../public/stadium.jpg') no-repeat fixed center;
     }
 
     h2{
